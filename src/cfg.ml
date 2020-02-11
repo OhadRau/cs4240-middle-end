277214ad7c28e845f13461259e9d968159dda22b
ohadrau <ohadrau@yahoo.com>
2020-02-10 19:52:35 -0500

    CFG: Add update_vertices
    This function is used for copy propagation, where we want to modify
    vertices. The challenge here is that vertices are immutable, so we
    can't just tell OCamlgraph to update a vertex's label. Instead, we
    need to schedule a list of changes to make to the graph and apply
    those in the update_vertices method. This works similar to how we
    remove vertices in deadcode elimination: we keep track of each
    vertex's replacement and when we update the edges in the graph, we
    copy that original edge to the replaced vertices. For example, if
    we have a vertex A and a vertex B and we want to replace A with A'
    and B with B': simply adding A' => B and A => B' edges won't work,
    since these will disappear as soon as we remove the old vertices.
    Instead, we need to use the mappings to apply this change to the
    final graph, so that we add the edge A' => B'.

--- src/cfg.ml
+++ src/cfg.ml
@@ -218,6 +218,32 @@ let remove_vertices g vs =
       failwith msg
   end vs
 
+let update_vertices g replacements =
+  (* Create a mapping of each old vertex to its new
+     "replacement" vertex *)
+  let mappings = Hashtbl.create (G.nb_vertex g) in
+  G.iter_vertex (fun v -> Hashtbl.add mappings v v) g;
+  (* Update the mappings with the user's requested replacements *)
+  List.iter (fun (k, v) -> Hashtbl.replace mappings k v) replacements;
+  (* For each vertex that needs to be deleted *)
+  List.iter begin fun (v, v') ->
+    G.add_vertex g v';
+    (* Find its predecessors/successors *)
+    let preds = G.pred_e g v
+    and succs = G.succ_e g v in
+    (* Update every edge for this vertex to go to the new vertex *)
+    List.iter begin fun (src, label, _) ->
+      let src' = Hashtbl.find mappings src in
+      G.add_edge_e g (src', label, v')
+    end preds;
+    List.iter begin fun (_, label, dst) ->
+      let dst' = Hashtbl.find mappings dst in
+      G.add_edge_e g (v', label, dst')
+    end succs;
+    (* And finally delete the vertex *)
+    G.remove_vertex g v
+  end replacements
+
 let hashtbl_of_cfg g =
   let hashtbl = Hashtbl.create (G.nb_vertex g) in
   let add_mapping v =
